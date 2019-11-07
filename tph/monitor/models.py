2653957cf926163010e04a41b81e0bf6a72fda3c
Mitsuhisa Tanaka <asihustim@gmail.com>
2019-11-08 08:24:48 +0900

    feat: define store data from BME280.
    BME280 は高精度に気圧、湿度、気温を計測するセンサーです。

--- tph/monitor/models.py
+++ tph/monitor/models.py
@@ -1,3 +1,18 @@
+"""Models definition."""
 from django.db import models
 
-# Create your models here.
+
+class BME280(models.Model):
+    """Store from BME280 sensor.
+
+    Store from BME280 sensor.
+    BME280 senses pressure, humidity and temperature.
+
+    See https://www.bosch-sensortec.com/bst/products/all_products/bme280
+
+    """
+
+    pressure = models.FloatField()
+    humidity = models.FloatField()
+    temperature = models.FloatField()
+    measure_datetime = models.DateTimeField(auto_now_add=True)
