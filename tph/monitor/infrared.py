3aedbe927e9cfb89a52618775b41df5be5559bd4
Mitsuhisa Tanaka <asihustim@gmail.com>
2019-11-12 07:38:15 +0900

    feat(RPi TPH Monitor): very very initial add, not yet complete.

---
+++ tph/monitor/infrared.py
@@ -0,0 +1,6 @@
+"""
+Controle samething electric devices via infrared remoto control.
+
+For more information to RPi TPH Monitor Rev2.
+https://www.indoorcorgielec.com/products/rpi-tph-monitor-rev2/
+"""
