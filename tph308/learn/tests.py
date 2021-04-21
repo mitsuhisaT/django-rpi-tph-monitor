d8b9f289875f41cf83cc03f2b34f5dde32697251
mitsuhisa tanaka <tanaka_m01@pci-tcn.jp>
2021-04-22 08:38:11 +0900

    migrate: delete old Django 3.0.18.

--- tph308/learn/tests.py
+++
@@ -1,7 +0,0 @@
-from django.test import TestCase
-from  learn.ml_tph_rnn import buildSequentialModel
-
-
-# Create your tests here.
-buildSequentialModel()
-buildSequentialModel(4, 2, 2, 1)
--- tph308/monitor/lcd.py
+++
@@ -1,6 +0,0 @@
-"""
-Controle LCD Class for the RPi TPH Monitor Rev2.
-
-For more information to RPi TPH Monitor Rev2.
-https://www.indoorcorgielec.com/products/rpi-tph-monitor-rev2/
-"""
