340667af64279817a3d76832f7e3e74aa27e7372
mitsuhisa tanaka <tanaka_m01@pci-tcn.jp>
2021-04-30 05:04:16 +0900

    chore: delete old migration work, after check on Raspberry Pi.

--- tph318/monitor/infrared.py
+++
@@ -1,6 +0,0 @@
-"""
-Controle samething electric devices via infrared remoto control.
-
-For more information to RPi TPH Monitor Rev2.
-https://www.indoorcorgielec.com/products/rpi-tph-monitor-rev2/
-"""
