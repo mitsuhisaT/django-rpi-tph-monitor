b1d4ee94395647cb96649aa524bbc6d75a147725
mitsuhisa tanaka <tanaka_m01@pci-tcn.jp>
2021-05-09 17:20:12 +0900

    chore: change database to MariaDB, because 'Database is locked' error.

--- tph/tph/settings.py
+++ tph/tph/settings.py
@@ -87,8 +87,12 @@ ASGI_APPLICATION = 'tph.routing.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': '/home/mitsu/.config/mariadb.cnf',
+        }
     }
 }
 
